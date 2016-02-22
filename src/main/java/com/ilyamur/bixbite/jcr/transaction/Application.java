5f5b854953092dc286d1b2f973e219d9ff3d76c7
Ilya_Muravyev <ilya.yarn@gmail.com>
2016-02-22 22:24:01 +0300

    Initial commit

---
+++ src/main/java/com/ilyamur/bixbite/jcr/transaction/Application.java
@@ -0,0 +1,33 @@
+package com.ilyamur.bixbite.jcr.transaction;
+
+import org.apache.jackrabbit.core.TransientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Repository;
+import javax.jcr.Session;
+
+/**
+ * Application.
+ *
+ * @author Ilya_Muravyev
+ */
+public class Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    /**
+     * Main method.
+     */
+    public static void main(String[] args) throws Exception {
+        Repository repository = new TransientRepository();
+        Session session = repository.login();
+        try {
+            String user = session.getUserID();
+            String name = repository.getDescriptor(Repository.REP_NAME_DESC);
+            LOG.info("Logged in as " + user + " to a " + name + " repository.");
+        } finally {
+            session.logout();
+        }
+    }
+}
